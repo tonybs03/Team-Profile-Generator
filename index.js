6a8320ec03fdab1c0735d00735b42972fcd8f2dd
Tony Fan <tony.bs.0303@gmail.com>
2022-06-13 14:26:04 -0400

    initial commit, created empty files

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
